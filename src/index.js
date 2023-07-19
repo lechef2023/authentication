c111ad61a050a0b8309c4eadd6d408842033c2a6
lechef2023 <lenyncondelechef@gmail.com>
2023-07-19 13:44:07 -0400

    Auth funcional

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import firebase from 'firebase/compat/app'; // Cambiamos la importación aquí
+import 'firebase/compat/auth'; // Cambiamos la importación aquí
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const firebaseConfig = {
+  apiKey: "AIzaSyCU73RxrfcH3JTpbf5H30nJX199zP0J6b8",
+  authDomain: "aulavirtual-1e3d2.firebaseapp.com",
+  projectId: "aulavirtual-1e3d2",
+  storageBucket: "aulavirtual-1e3d2.appspot.com",
+  messagingSenderId: "369023069546",
+  appId: "1:369023069546:web:52ab26d3e10411aa8f4913",
+  measurementId: "G-E4CJ49KH0B"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
